2da515392c8da34a6a9946567c2b688918d3d514
yona-hammouchi <yona.hammouchi@laplateforme.io>
2024-12-03 10:03:33 +0100

    init project

--- test.py
+++ test.py
@@ -1,24 +1,21 @@
+import tkinter
 import tkinter as tk
 
-# Créer la fenêtre principale
+def draw_grid():
+    button = tkinter.Button(root)
+    button.pack()
+
+ #creer la fenêtre de jeu
 root = tk.Tk()
+
+#perso de la fenetre
 root.title("Tic Tac Toe")
+root.minsize(500, 500)
 
-# Configuration de la grille de boutons
-buttons = [[None for _ in range(3)] for _ in range(3)]  # Grille 3x3
-
-def on_click(row, col):
-    buttons[row][col].config(text="X")  # Exemple : placer un X au clic
-    buttons[row][col].config(text=("Y"))
-
-
-
-# Création des boutons
-for i in range(3):
-    for j in range(3):
-        buttons[i][j] = tk.Button(root, text="", font=("Arial", 24), width=5, height=2,
-                                  command=lambda r=i, c=j: on_click(r, c))
-        buttons[i][j].grid(row=i, column=j)  # Positionner les boutons sur la grille
-
-# Démarrer la boucle d'événements
+draw_grid()
 root.mainloop()
+
+
+
+
+
