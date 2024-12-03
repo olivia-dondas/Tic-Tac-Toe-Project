5dde8eefea61496707646a8b3eb0ce5b0f22ade2
Your Name <you@example.com>
2024-12-03 11:02:26 +0100

    Morpion Marvin

--- marvin.py
+++ marvin.py
@@ -1,53 +1,24 @@
-# Création d'un affichage 
 
+                                                                        #Variable de base 
 
-
-
-                                    #///// V1
-# def affichage() :
-#     bordure1 = " _   _   _   _   _  _"
-#     print(bordure1)
-#     return affichage
-
-# affichage()
-
-# def affichage2():
-#     for bd1 in range (0, 5) :
-#         bd1 = "|                    |"
-#         print(bd1)
-    
-# affichage2()
-# affichage()
-
-                                    #///// V2
-
-
-# def affichage_grille():
-#     tiret = "__"
-#     print(tiret*20)
-#     for i in range (1) :
-#         i = "|     1     |       2       |     3     |"
-#         print(i)
-#         print(tiret*20)
-#     for j in range (1) :
-#         j =  "|     4     |       5       |     6     |"
-#         print(j)
-#         print (tiret*20)
-#     for h in range(1):
-#         h= "|     7     |       8       |     9     |"
-#         print (h)
-#         print(tiret*20)
-
-
-
-def jouer():
-    choix_joueur()
-    affichage_grille()
 
 grille = ["-","-","-","-","-","-","-","-","-",]
 
 joueur_actuel = ""
+fin_jeu = False
+gagnant = ""
 
+                                                                #Fonction de toutes les fonctions
+def jouer():
+    choix_joueur()
+    affichage_grille()
+    while fin_jeu == False :
+        tour(joueur_actuel)
+        verifier_fin_jeu()
+        joueur_suivant()
+    resultat()
+
+                                                            #Fonction pour le choix du joueur 
 def choix_joueur():
     global joueur_actuel
     joueur_actuel = input("Veuillez choisir soit une croix (X), soit un rond (O): " )
@@ -60,10 +31,11 @@ def choix_joueur():
         elif joueur_actuel == "O":
             print("Vous avez choisi O, le joueur 2 aura X")
             break
+
         else : 
             joueur_actuel = input("Veuillez choisir soit une croix (X), soit un rond (O) " )
 
-    
+                                            #Fonction de l'affichage de grille
 def affichage_grille():
     print("\n")
     print("-----------------")
@@ -74,357 +46,86 @@ def affichage_grille():
     print("|", grille[6], "|","|", grille[7], "|","|", grille[8], "|                |   7   |   8   |   9   |")
     print("-----------------")
 
-jouer()
-
-                            # Morpion SCOTT ET SAMMY
-# from random import *
-
-# """L'ultime grille (buggé)"""
-
-
-# """Séléction du mode de jeu."""
-
-
-# #Début de la "boucle de démarrage"
-# status = False
-
-# while status != True:
-
-#     print( 
-#         "Séléctionnez le mode de jeu : " "\n"
-#         "1 : Joueur contre Joueur." "\n"
-#         "2 : Joueur contre IA" "\n"
-#     )
-
-#     #Séléction du mode
-#     select = int(input("Mode choisi : "))
-    
-#     """MODE JOUEUR CONTRE JOUEUR"""
-
-#     if select == 1: #Joueur contre Joueur
-
-#         """Séléction camp"""
-
-#         print(
-#             "\n"
-#             "Veuillez séléctionner votre camp : " "\n"
-#             "Croix = O" "\n"
-#             "Ronds = X"
-#         )
-
-#         joueur_1 = input("Joueur 1 : ")
-#         joueur_2 = None
-
-#         if joueur_1 == "X":
-#             joueur_2 = "O"
-
-#         if joueur_1 == "O":
-#             joueur_2 = "X"
-#         print("Joueur 2 :",joueur_2)
-    
-
-#     """MODE JOUEUR CONTRE AI"""
-
-#     if select == 2: #Joueur contre AI
-           
-#         """Selection camp"""
-
-#         print(
-#             "\n"
-#             "Veuillez séléctionner votre camp : " "\n"
-#             "Croix = O" "\n"
-#             "Ronds = X"
-#         )
-
-#         Humain = input("Joueur 1 : ")
-#         Ai = None
-
-#         if Humain == "X":
-#             Ai = "O"
-
-#         if Humain == "O":
-#             Ai = "X"
-#         print("AI :", Ai)
-
-#         print("En développement. L'AI est très space...")
-#         break
-
-#     status = True #Fin de la boucle de séléction
-
-
-# """Formation de la grille"""
-
-
-# #Création de la grille
-# grille = [0,1,2,3,4,5,6,7,8]
-
-# def morpionnage(liste:list):
-#     """Formatte la liste grille afin d'avoir 
-#     un morpion affiché dans le terminal."""
-#     print(
-#     f"{liste[6]} | {liste[7]} | {liste[8]}" "\n"
-#     "---------" "\n"
-#     f"{liste[3]} | {liste[4]} | {liste[5]}" "\n"
-#     "---------" "\n"
-#     f"{liste[0]} | {liste[1]} | {liste[2]}" "\n"
-#     )
-
-
-# """Gestion de la partie"""
-
-# tour = 1 #Note : il y a 9 tours
-
-# """JOUEUR CONTRE JOUEUR"""
-
-
-# if select == 1: #Boucle de jeu Joueur contre Joueur
-#     i = 1
-#     while i < 10:
-
-#         """Au tour du joueur 1"""
-
-#         print(morpionnage(grille))
-
-#         j1 = int(input("Joueur 1, position : "))
-    
-#         for c in grille:
-#             if j1 == c:
-#                 grille[c] = joueur_1
-        
-#         #VICTOIRES LIGNES VERTICALES
-
-#         if grille[0] == joueur_1 and grille[3] == joueur_1 and grille[6] == joueur_1:
-#             print("\n""joueur 1 a gagné")
-#             break
-    
-#         if grille[1] == joueur_1 and grille[4] == joueur_1 and grille[7] == joueur_1:
-#             print("\n""joueur 1 a gagné")
-#             break
-    
-#         if grille[2] == joueur_1 and grille[5] == joueur_1 and grille[8] == joueur_1:
-#             print("\n""joueur 1 a gagné")
-#             break
-
-#         #VICTOIRES LIGNES HORIZONTALES
-
-#         if grille[0] == joueur_1 and grille[1] == joueur_1 and grille[2] == joueur_1:
-#             print("\n""joueur 1 a gagné")
-#             break
-    
-#         if grille[3] == joueur_1 and grille[4] == joueur_1 and grille[5] == joueur_1:
-#             print("\n""joueur 1 a gagné")
-#             break
-
-#         if grille[6] == joueur_1 and grille[7] == joueur_1 and grille[8] == joueur_1:
-#             print("\n""joueur 1 a gagné")
-#             break
-
-#         #VICTOIRES LIGNES DIAGONALES
-
-#         if grille[2] == joueur_1 and grille[4] == joueur_1 and grille[6] == joueur_1:
-#             print("\n""joueur 1 a gagné")
-#             break
-
-#         if grille[0] == joueur_1 and grille[4] == joueur_1 and grille[8] == joueur_1:
-#             print("\n""Joueur 1 a gagné")
-#             break
-
-#         """Au tour du joueur 2"""
-
-#         print(morpionnage(grille))
-
-#         j2 = int(input("Joueur 2, position : "))
-    
-#         for r in grille:
-#             if j2 == r:
-#                 grille[r] = joueur_2
-
-#         """E N D I N G S  R O N D"""
-
-#         #VICTOIRES LIGNES VERTICALES
-
-#         if grille[0] == joueur_2 and grille[3] == joueur_2 and grille[6] == joueur_2:
-#             print("\n""joueur 2 a gagné")
-#             break
-    
-#         if grille[1] == joueur_2 and grille[4] == joueur_2 and grille[7] == joueur_2:
-#             print("\n""joueur 2 a gagné")
-#             break
-    
-#         if grille[2] == joueur_2 and grille[5] == joueur_2 and grille[8] == joueur_2:
-#             print("\n""joueur 2 a gagné")
-#             break
-
-#         #VICTOIRES LIGNES HORIZONTALES
-
-#         if grille[0] == joueur_2 and grille[1] == joueur_2 and grille[2] == joueur_2:
-#             print("\n""joueur 2 a gagné")
-#             break
-    
-#         if grille[3] == joueur_2 and grille[4] == joueur_2 and grille[5] == joueur_2:
-#             print("\n""joueur 2 a gagné")
-#             break
-
-#         if grille[6] == joueur_2 and grille[7] == joueur_2 and grille[8] == joueur_2:
-#             print("\n""joueur 2 a gagné")
-#             break
-
-#         #VICTOIRES LIGNES DIAGONALES
-
-#         if grille[2] == joueur_2 and grille[4] == joueur_2 and grille[6] == joueur_2:
-#             print("\n""joueur 2 a gagné")
-#             break
-
-#         if grille[0] == joueur_2 and grille[4] == joueur_2 and grille[8] == joueur_2:
-#             print("\n""Joueur 2 a gagné")
-#             break
-
-#         i += 1
-#         if i >= 9:
-#             print("Match nul. c'est nul.")
-
-
-
-# """JOUEUR CONTRE INTELIGENCE ARTIFICIELLE IDIOTE"""
-
-# grille = [0,1,2,3,4,5,6,7,8,9]
-
-# disponible = [0,1,2,3,4,5,6,7,8,9]
-
-# if select == 2:
-
-#     """     Deroulement : comment l'ia fonctionne
-
-# Donc on a deux listes :
-# grille : la ou on joue
-# available : places libres dans la liste
-
-# Qu'on soit d'accord, les croix et les ronds sont des strings.("X" et "O")
-# Ca veut dire que l'ia va choisir un entier entre 1 et 8 dans la liste available.
-# Si l'entier est présent dans la liste
-# alors il place sa croix/rond
-# Pour éviter les conflits, chaque place choisi par l'ia et le joueur sera supprimé
-# de la liste available grâce à la fonction del
-# *Note marrante : En voulant éviter ce conflit j'ai créer d'autres conflits    
-#     """
-    
-#     tour = 0
-    
-#     while disponible != []:
-        
-#         """Au tour du joueur"""
-
-#         #print(morpionnage(grille))
-        
-#         #print(available)
-
-#         print(morpionnage(grille))
-
-#         Choix_Joueur = int(input("Choisissez une place disponible : "))
-        
-#         if Choix_Joueur in grille:
-#             #print("Il est la le frère.")
-#             grille[Choix_Joueur] = Humain
-#             #print(morpionnage(grille))
-#             del(disponible[Choix_Joueur-tour])
-#             #print(disponible)
-            
-#             #print("Tour = ", tour)
-            
-
-#         #VICTOIRES LIGNES VERTICALES
-
-#         if grille[0] == Humain and grille[3] == Humain and grille[6] == Humain:
-#             print("\n""L'Humain a gagné. Logique...")
-#             break
-    
-#         if grille[1] == Humain and grille[4] == Humain and grille[7] == Humain:
-#             print("\n""L'Humain a gagné. Logique...")
-#             break
-    
-#         if grille[2] == Humain and grille[5] == Humain and grille[8] == Humain:
-#             print("\n""L'Humain a gagné. Logique...")
-#             break
-
-#         #VICTOIRES LIGNES HORIZONTALES
-
-#         if grille[0] == Humain and grille[1] == Humain and grille[2] == Humain:
-#             print("\n""L'Humain a gagné. Logique...")
-#             break
-    
-#         if grille[3] == Humain and grille[4] == Humain and grille[5] == Humain:
-#             print("\n""L'Humain a gagné. Logique...")
-#             break
-
-#         if grille[6] == Humain and grille[7] == Humain and grille[8] == Humain:
-#             print("\n""L'Humain a gagné. Logique...")
-#             break
-
-#         #VICTOIRES LIGNES DIAGONALES
-
-#         if grille[2] == Humain and grille[4] == Humain and grille[6] == Humain:
-#             print("\n""L'Humain a gagné. Logique...")
-#             break
-
-#         if grille[0] == Humain and grille[4] == Humain and grille[8] == Humain:
-#             print("\n""L'Humain a gagné. Logique...")
-#             break
-
-#         tour += 1
-            
-
-#         """Au tour de l'IA"""
-
-
-#         #print(morpionnage(grille))
-
-#         Choix_ia = choice(disponible)
-#         #print("L'ia a choisi : ",Choix_ia)
-#         if Choix_ia in disponible:
-#             grille[Choix_ia-tour] = Ai
-#             del(disponible[Choix_ia-tour])
-
-#         tour += 1
-
-#         #VICTOIRES LIGNES VERTICALES
-
-#         if grille[0] == Ai and grille[3] == Ai and grille[6] == Ai:
-#             print("\n""L'intelligence artificielle a gagné, un exploit.")
-#             break
-    
-#         if grille[1] == Ai and grille[4] == Ai and grille[7] == Ai:
-#             print("\n""L'intelligence artificielle a gagné, un exploit.")
-#             break
-    
-#         if grille[2] == Ai and grille[5] == Ai and grille[8] == Ai:
-#             print("\n""L'intelligence artificielle a gagné, un exploit.")
-#             break
-
-#         #VICTOIRES LIGNES HORIZONTALES
-
-#         if grille[0] == Ai and grille[1] == Ai and grille[2] == Ai:
-#             print("\n""L'intelligence artificielle a gagné, un exploit.")
-#             break
-    
-#         if grille[3] == Ai and grille[4] == Ai and grille[5] == Ai:
-#             print("\n""L'intelligence artificielle a gagné, un exploit.")
-#             break
-
-#         if grille[6] == Ai and grille[7] == Ai and grille[8] == Ai:
-#             print("\n""L'intelligence artificielle a gagné, un exploit.")
-#             break
-
-#         #VICTOIRES LIGNES DIAGONALES
-
-#         if grille[2] == Ai and grille[4] == Ai and grille[6] == Ai:
-#             print("\n""L'intelligence artificielle a gagné, un exploit.")
-#             break
-
-#         if grille[0] == Ai and grille[4] == Ai and grille[8] == Ai:
-#             print("\n""L'intelligence artificielle a gagné, un exploit.")
-#             break
-
-        
+                                                                        #Fonction des tours des joueurs
+
+def tour(joueur) :
+    print("C'est le tour dur joueur : ", joueur )
+    pos = input ("Veuillez sélectionner un espace vide sur la grille entre 1 et 9 : " )
+
+    valide = False
+    while valide == False :
+
+        while pos not in ["1", "2", "3","4", "5", "6","7", "8", "9"] :                  #Définir qu'il faut choisir une position compris entre 1 et 9
+            pos = input ("Veuillez sélectionner un espace vide sur la grille entre 1 et 9 : " )
+        pos = int(pos) - 1
+
+        if grille [pos] == "-" :
+            valide = True
+
+        else :
+            print("Vous ne pouvrez pas accéder à cette position")       #Print un message d'erreur quand l'on ne choisit pas une position connue
+
+        grille[pos] = joueur
+        affichage_grille()
+
+def verifier_fin_jeu():
+    verifier_victoire()                                                         # Fonction qui vérifie la fin de jeu
+    verifier_match_nul()
+
+def verifier_victoire() :
+                                                    #Global = 
+    global fin_jeu
+    global gagnant                                                                                                          
+
+                                                                                            #Vérification de victoire en vertical 
+    if grille[0] == grille [1] == grille [2] and grille[2] != "-" :
+        fin_jeu = True
+        gagnant = grille[1]
+    if grille[3] == grille [4] == grille [5] and grille[5] != "-" :
+        fin_jeu = True
+        gagnant = grille[2]
+    if grille[6] == grille [7] == grille [8] and grille[8] != "-" :
+        fin_jeu = True
+        gagnant = grille[8]
+    if grille[0] == grille [3] == grille [6] and grille[2] != "-" :
+        fin_jeu = True
+        gagnant = grille[6]
+    if grille[1] == grille [4] == grille [7] and grille[7] != "-" :
+        fin_jeu = True
+        gagnant = grille[7]
+    if grille[2] == grille [5] == grille [8] and grille[8] != "-" :
+        fin_jeu = True
+        gagnant = grille[8]
+
+                                                                                            #Vérification de victoire en Diagonale
+    if grille[0] == grille [4] == grille [4] and grille[8] != "-" :
+        fin_jeu = True                                                                        
+        gagnant = grille[8]
+    if grille[2] == grille [4] == grille [6] and grille[6] != "-" :
+        fin_jeu = True
+        gagnant = grille[6]
+
+def verifier_match_nul() :                                          #CONDITION DE MATCH NUL
+    global fin_jeu
+    if "-" not in grille :
+        fin_jeu = True
+
+def joueur_suivant() :                                              #Fonction qui permet de définir quelle joueur doit jouer
+    global joueur_actuel
+    if joueur_actuel == "X" :
+        joueur_actuel = "O"
+    else :
+        joueur_actuel = "X"
+
+def resultat () :                                                                #Permet de donner un résultat si les condition sont réunis
+    if gagnant == "X" or gagnant == "O" :
+        print("Le joueur : ", gagnant, "a gagné")
+    else :
+        print("Match nul. " )
+
+
+
+jouer()                 #Appeler la fonction jouer
 
 
 
